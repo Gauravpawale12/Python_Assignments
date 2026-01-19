ab9056cace39bde60da468278a82bae24e179d8f
Gauravpawale12 <132771629+Gauravpawale12@users.noreply.github.com>
2026-01-19 10:01:18 +0530

    Delete Python_Assignment_1 directory

--- Python_Assignment_1/Assign_1_Q_1.py
+++
@@ -1,8 +0,0 @@
-# Write a program which contains one function named as Display()
-# that prints "Jay Ganesh" on console
-
-def main():
-    print("Jay Ganesh...")
-    
-if __name__ == "__main__":
-    main()
