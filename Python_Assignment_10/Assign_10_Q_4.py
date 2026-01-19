51f58e052edd2e59316cbbc47016ba61f4554696
Gauravpawale12 <132771629+Gauravpawale12@users.noreply.github.com>
2026-01-19 11:43:38 +0530

    Add C implementation for printing even numbers

--- Python_Assignment_10/Assign_10_Q_4.py
+++ Python_Assignment_10/Assign_10_Q_4.py
@@ -23,4 +23,33 @@ def main():
 
     
 if __name__ == "__main__":
+
     main()
+
+'''
+#include<stdio.h>
+
+int Even_Number(int iValue)
+{
+   int iCnt = 0;
+   for(iCnt = 1;iCnt <= iValue;iCnt++)
+   {
+       if(iCnt % 2 == 0)
+       {
+           printf("%d\n",iCnt);
+       }
+   }
+ }
+int main()
+{
+    int iNo = 0;
+    
+    printf("Enter the number : \n");
+    scanf("%d",&iNo);
+    
+    Even_Number(iNo);
+    
+    return 0;
+}
+
+'''
