a8fc99e600c2990c7acbd3f8fad68d9c7651eb48
Gauravpawale12 <132771629+Gauravpawale12@users.noreply.github.com>
2026-01-19 11:24:21 +0530

    divisible check program in Python and C

--- Python_Assignment_9/Assign_9_Q_5.py
+++ Python_Assignment_9/Assign_9_Q_5.py
@@ -23,4 +23,35 @@ def main():
 
     
 if __name__ == "__main__":
+
     main()
+
+
+'''
+#include<stdio.h>
+
+int Check_Divisible(int iValue)
+{
+    if((iValue % 3 == 0) && (iValue % 5 == 0))
+    {
+        
+        printf("%d is Divisible by 3 and 5\n",iValue);
+    }
+    else
+    {
+        printf("%d is NOT Divisible by 3 and 5\n");
+    }
+}
+int main()
+{
+    int iNo = 0;
+    
+    printf("Enter the number : \n");
+    scanf("%d",&iNo);
+    
+    Check_Divisible(iNo);
+    
+    return 0;
+}
+
+'''
